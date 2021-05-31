590d919c1a7fe9d37e55b2d1db837500d5a227a1
Nadine0109 <nadine0109@ya.ru>
2021-05-31 15:53:25 +0500

    Initial commit with failure

---
+++ src/test/java/PostmanEchoTesting.java
@@ -0,0 +1,22 @@
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTesting {
+
+    @Test
+    public void shouldCheckPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("I write this text to check it works") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */));
+    }
+}
